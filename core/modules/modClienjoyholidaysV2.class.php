1713f10a4474bcc3597ac1fdf73f4279dd44b0d2
lucas <lucas.mantegari@atmlucas.mantegari@atm-consulting.fr>
2023-02-02 09:59:05 +0100

    correctif 02/02

--- core/modules/modClienjoyholidaysV2.class.php
+++ core/modules/modClienjoyholidaysV2.class.php
@@ -374,7 +374,7 @@ class modClienjoyholidaysV2 extends DolibarrModules
             'fk_menu'=>'fk_mainmenu=clienjoyholidaysv2',
             // This is a Left menu entry
             'type'=>'left',
-            'titre'=>'Liste des formules de voyage',
+            'titre'=>'List voyage',
             'mainmenu'=>'clienjoyholidaysv2',
             'leftmenu'=>'clienjoyholidaysv2_voyage',
             'url'=>'/clienjoyholidaysv2/voyage_list.php',
@@ -394,7 +394,7 @@ class modClienjoyholidaysV2 extends DolibarrModules
             'fk_menu'=>'fk_mainmenu=clienjoyholidaysv2,fk_leftmenu=clienjoyholidaysv2_voyage',
             // This is a Left menu entry
             'type'=>'left',
-            'titre'=>'Nouveau voyage',
+            'titre'=>'New voyage',
             'mainmenu'=>'clienjoyholidaysv2',
             'leftmenu'=>'clienjoyholidaysv2_voyage',
             'url'=>'/clienjoyholidaysv2/voyage_card.php?action=create',
